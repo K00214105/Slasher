3081027a36108ec8fd2123ff9806feef089330be
K00214105 <K00214105@student.lit.ie>
2018-11-23 14:33:33 +0100

    Adding pages

---
+++ Rythm Slasher/Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    private Vector3 increment = new Vector3(0, 0, .01f);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(increment);
+	}
+}
